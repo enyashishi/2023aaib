173b10ab2820a912c2b47cc79c5457abbd6be723
enyashishi <enyatseng46@gmail.com>
2024-01-12 11:29:33 +0800

    week18

---
+++ week18/week18-4.py
@@ -0,0 +1,16 @@
+# 資料型別
+a=10
+print(type(a), a)
+a='123'
+print(type(a), a)
+a=3.14
+print(type(a), a)
+# 運算
+a = 1+2
+print(a)
+# 字串
+a= 'Hello'
+b= 'World'
+print(a+b)
+print(a*5)
+print(a[0],a[1],a[-1])
---
+++ week18/week18-5.py
@@ -0,0 +1,11 @@
+a=[100,200]
+c,d=a
+
+print(c)
+print(d)
+s='Hello'
+t={100:200,300:400} # 字典
+t={300} # 集合
+
+# chapter 4 if 判斷
+# chapter 5 for 迴圈
